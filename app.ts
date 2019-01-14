f0f90ddef6a1f46ffb0847c3a63b561fcd5bd42e
Roman Batsenko <rbatsenko@gmail.com>
2019-01-14 18:47:22 +0100

    learning types 3

--- app.ts
+++ app.ts
@@ -1,5 +1,14 @@
+//string
 let myName = 'Roman';
 
+//number
 let myRealAge: number;
 //myRealAge = '24';
 myRealAge = 24;
+
+//array
+let hobbies: any[] = ['Web Development', 'Climbing'];
+hobbies = [100];
+//hobbies = 100;
+
+console.log(hobbies[0]);
