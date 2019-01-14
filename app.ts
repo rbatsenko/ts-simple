c2cc33eaffe278b4193beb1fbb8d45472c20676b
Roman Batsenko <rbatsenko@gmail.com>
2019-01-14 19:03:01 +0100

    learning types 5

--- app.ts
+++ app.ts
@@ -13,3 +13,17 @@ hobbies = [100];
 
 //tuples
 let address: [string, number] = ['Superstreet', 99];
+
+//enums
+enum Color {
+  Grey, // 0
+  Green = 100, // 1
+  Blue = 2// 2
+}
+
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+//any
+let car: any = 'BMW';
+console.log(car);
